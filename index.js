cfcc67b523f19da5f0656abec831671076567d0f
Demetrio Torgan <demetriotorgan@hotmail.com>
2024-01-29 16:29:16 -0300

    first commit

--- index.js
+++ index.js
@@ -64,10 +64,32 @@ app.get('/person/:id', async(req,res)=>{
   const id = req.params.id
   try {
     const person = await Person.findOne({_id:id})
+      //validando a busca
+        if(!person){
+          res.status(422).json({message: 'Usuário não encontrado'})
+          return
+        }
     res.status(200).json(person)
   } catch (error) {
     res.status(500).json({erro:error})
   }
+})
+
+//rota DELETE --------------
+app.delete('/person/:id', async(req,res)=>{
+  const id = req.params.id
+  const person = await Person.findOne({_id:id})
+    if(!person){
+      res.status(422).json({mensagem: 'Usuario não encontrado'})
+      return
+    }
+
+    try {
+      await Person.deleteOne({_id:id})
+      res.status(200).json({mensagem:'Usuário excluido com sucesso'})
+    } catch (error) {
+      res.status(500).json({erro:error})
+    }
 })
 
 //entregando uma porta
